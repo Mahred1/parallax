18c338fd0b73097d89e449d8e601c17ed15f2866
Mahred1 <mredwan.kemal@gmail.com>
2025-08-14 09:42:54 +0300

    fixed the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-
 export default function Home() {
   return (
-
-   <main className="">
-    {/* Hero section */}
-    <section className="h-screen object-fill hero-bg">
-
-    </section>
-   </main>
+    <main className="">
+      {/* Hero section */}
+      <section className="h-screen hero-bg"></section>
+    </main>
   );
 }
